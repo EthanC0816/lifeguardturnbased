c5bc10719c97bc8e311aa045f6e38ff1693536cb
Ethan <ElzueYT@gmail.com>
2026-02-09 13:47:07 +0000

    WORKING GAMEPLAY

--- Assets/EthanSTANKY/Scripts/TurnManager.cs
+++ Assets/EthanSTANKY/Scripts/TurnManager.cs
@@ -5,22 +5,83 @@ public class TurnManager : MonoBehaviour
 {
     public List<PlayerController> players;
     public int currentPlayerIndex = 0;
+
+    public int totalRounds = 10;
+    public int currentRound = 1;
+
+    private int turnsTakenThisRound = 0;
+    private int activePlayersAtRoundStart = 0;
+
     void Start()
     {
         StartTurn();
     }
 
-   void StartTurn()
+    void StartTurn()
     {
+       
+        if (turnsTakenThisRound == 0)
+        {
+            activePlayersAtRoundStart = 0;
+            foreach (var p in players)
+                if (!p.hasFinished)
+                    activePlayersAtRoundStart++;
+        }
+
         players[currentPlayerIndex].StartPlayerTurn();
     }
 
     public void EndTurn()
     {
         players[currentPlayerIndex].isMyTurn = false;
+        turnsTakenThisRound++;
 
-        currentPlayerIndex = (currentPlayerIndex + 1 ) % players.Count;
+     
+        int finishedPlayers = 0;
+        foreach (var p in players)
+            if (p.hasFinished)
+                finishedPlayers++;
+
+        
+        if (finishedPlayers >= players.Count - 1)
+        {
+            EndGame();
+            return;
+        }
+
+        
+        if (turnsTakenThisRound >= activePlayersAtRoundStart)
+        {
+            currentRound++;
+            turnsTakenThisRound = 0;
+
+            Debug.Log($"--- ROUND {currentRound} STARTING ---");
+
+            if (currentRound > totalRounds)
+            {
+                EndGame();
+                return;
+            }
+        }
+
+        
+        do
+        {
+            currentPlayerIndex = (currentPlayerIndex + 1) % players.Count;
+        }
+        while (players[currentPlayerIndex].hasFinished);
+
         StartTurn();
     }
 
+    void EndGame()
+    {
+        Debug.Log("---- GAME ENDED ----");
+
+        foreach (var p in players)
+            Debug.Log($"{p.name} finished with {p.money} coins");
+
+        foreach (var p in players)
+            p.spacebar.action.Disable();
+    }
 }
