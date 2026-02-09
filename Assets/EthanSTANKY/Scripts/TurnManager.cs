adf481b2477d87fb0151f58a827799734ea7d493
Ethan <ElzueYT@gmail.com>
2026-02-09 10:46:06 +0000

    rat rat rat

---
+++ Assets/EthanSTANKY/Scripts/TurnManager.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public List<PlayerController> players;
+    private int currentPlayerIndex = 0;
+    void Start()
+    {
+        StartTurn();
+    }
+
+   void StartTurn()
+    {
+        players[currentPlayerIndex].StartPlayerTurn();
+    }
+
+    public void EndTurn()
+    {
+
+        currentPlayerIndex = (currentPlayerIndex + 1 ) % players.Count;
+        StartTurn();
+    }
+
+}
