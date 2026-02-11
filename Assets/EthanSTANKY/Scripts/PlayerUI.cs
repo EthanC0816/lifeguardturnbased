12cc0bb0425c302187e8adc76cd2a974bb359c1d
Ethan <ElzueYT@gmail.com>
2026-02-11 15:42:47 +0000

    aaaaaa

--- Assets/EthanSTANKY/Scripts/PlayerUI.cs
+++ Assets/EthanSTANKY/Scripts/PlayerUI.cs
@@ -3,19 +3,20 @@ using System.Collections;
 
 public class PlayerUI : MonoBehaviour
 {
-    private RectTransform rect;
+    private RectTransform playerRect;
     [SerializeField] private RectTransform user;
+    [SerializeField] private RectTransform coinRect;
 
     private void Awake()
     {
-        rect = GetComponent<RectTransform>();
-        rect.pivot = new Vector2(0f, 0.5f); // LEFT center
+        playerRect = GetComponent<RectTransform>();
+        playerRect.pivot = new Vector2(0f, 0.5f); // LEFT center
         user.pivot = new Vector2(0.5f, 0f);
     }
 
     private void Start()
     {
-        rect.localScale = new Vector3(0f, 1f, 1f);
+        playerRect.localScale = new Vector3(0f, 1f, 1f);
         user.localScale = new Vector3(1f,0f,1f);
         StartCoroutine(ShowBackgroundAfterDelay());
         StartCoroutine(ShowUsernameAfterDelay());
@@ -33,9 +34,15 @@ public class PlayerUI : MonoBehaviour
         PlayerUser();
     }
 
+    private IEnumerator ShowCoinsBackgroundAfterDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        PlayerBackground();
+    }
+
     public void PlayerBackground()
     {
-        rect.LeanScaleX(1f, 0.5f).setEaseInCirc();
+        playerRect.LeanScaleX(1f, 0.5f).setEaseInCirc();
     }
 
     public void PlayerUser()
